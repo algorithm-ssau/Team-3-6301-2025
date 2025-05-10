475883faa930a01e3a1d8c3048fc3545e5c7b70f
Sm1rnov10 <160774950+Sm1rnov10@users.noreply.github.com>
2025-05-10 11:05:28 +0000

    добавление контроллера для работы с бэком

---
+++ src/main/resources/db.migration/java/com.example.demo/controller/SubjectController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.SubjectService;
+import com.example.demo.model.Subject;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/subjects")
+@RequiredArgsConstructor
+public class SubjectController {
+    private final SubjectService service;
+
+    // GET /api/subjects — вернуть весь список
+    @GetMapping
+    public List<Subject> getAll() {
+        return service.findAll();
+    }
+
+    // POST /api/subjects — добавить новый Subject
+    @PostMapping
+    public ResponseEntity<Subject> create(@RequestBody Subject subject) {
+        Subject saved = service.create(subject);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(saved);
+    }
+}
